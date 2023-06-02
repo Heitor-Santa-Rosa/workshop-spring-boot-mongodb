3fe12d7f65d490add4a6586964b0b577773e9d23
Heitor Santa Rosa da Silva <heitor_es@hotmail.com>
2023-06-02 18:00:14 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/heitorsantarosa/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.heitorsantarosa.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.heitorsantarosa.workshopmongo.domain.User;
+import com.heitorsantarosa.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
