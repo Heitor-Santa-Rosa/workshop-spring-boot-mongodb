87090f650677dab2e05ec48432cb867ae4f58d84
Heitor Santa Rosa da Silva <heitor_es@hotmail.com>
2023-06-04 12:32:44 -0300

    Obtendo um post por id

---
+++ src/main/java/com/heitorsantarosa/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.heitorsantarosa.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.heitorsantarosa.workshopmongo.domain.Post;
+import com.heitorsantarosa.workshopmongo.repository.PostRepository;
+import com.heitorsantarosa.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Optional<Post> findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+	
+}
