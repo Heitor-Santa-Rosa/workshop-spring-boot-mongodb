1ef49c0031158970c2edcb332ec55d82a5eddc4d
Heitor Santa Rosa da Silva <heitor_es@hotmail.com>
2023-06-04 11:12:36 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/heitorsantarosa/workshopmongo/services/UserService.java
+++ src/main/java/com/heitorsantarosa/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.heitorsantarosa.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,24 +13,26 @@ import com.heitorsantarosa.workshopmongo.services.exception.ObjectNotFoundExcept
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
-		User user = repo.findOne(id);
-		if (user == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado");			
-		}
-		return user;
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
 	}
 	
 	public User fromDTO(UserDTO objDto) {
