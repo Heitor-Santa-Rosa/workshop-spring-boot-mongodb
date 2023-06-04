6fc11bf2fbcb660c542b765b8a8cb0ba0f474dad
Heitor Santa Rosa da Silva <heitor_es@hotmail.com>
2023-06-04 14:43:50 -0300

    Consulta com vários critérios

--- src/main/java/com/heitorsantarosa/workshopmongo/resources/PostResource.java
+++ src/main/java/com/heitorsantarosa/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.heitorsantarosa.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,10 +28,23 @@ public class PostResource {
 		Optional<Post> obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
 	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
  	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
-	}	
+	}
+	
+	@RequestMapping(value="/fullSearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue = "") String text,
+ 			@RequestParam(value="minDate", defaultValue = "") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 }
